5844daba605018bbf32987bf8042ff8b32942f57
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2019-01-21 12:21:19 +0100

    new calendars

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -5,9 +5,8 @@ const OpenTimestamps = window.OpenTimestamps
 
 // an empty list would be equivalent to the default calendars
 const calendarsList = [
-    //'http://test-calendar.aniasafe.it:80',  // testnet
-    //'http://calendar.aniasafe.it:80',       // mainet
-    'http://calendar.irsa.it:80',           // testnet
+    'http://test-calendar.aniasafe.it:80',  // testnet
+    'http://calendar.aniasafe.it:80',       // mainet
     'https://alice.btc.calendar.opentimestamps.org', 
     'https://bob.btc.calendar.opentimestamps.org',
     'https://finney.calendar.eternitywall.com'
@@ -15,9 +14,8 @@ const calendarsList = [
 
 // an empty list is not acceptable here
 const wcalendars = [
-    //'http://test-calendar.aniasafe.it:80',  // testnet
-    //'http://calendar.aniasafe.it:80',       // mainet
-    'http://calendar.irsa.it:80',           // testnet
+    'http://test-calendar.aniasafe.it:80',  // testnet
+    'http://calendar.aniasafe.it:80',       // mainet
     'https://alice.btc.calendar.opentimestamps.org',
     'https://bob.btc.calendar.opentimestamps.org',
     'https://finney.calendar.eternitywall.com'
